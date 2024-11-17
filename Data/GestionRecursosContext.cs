d923e9fc3dd12031c5701cae4eeee4e4f06f9325
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-16 22:13:17 -0500

    configuración modelos y carpeta data

---
+++ Data/GestionRecursosContext.cs
@@ -0,0 +1,15 @@
+﻿using GestionRecursosHumanos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionRecursosHumanos.Data
+{
+    public class GestionRecursosContext : DbContext
+    {
+        public DbSet<Beneficios> Beneficio { get; set;}
+        public DbSet<Cargos> Cargo { get; set;}
+        public DbSet<Departamentos> Departamento { get; set;}
+        public DbSet<Empleados> Empleado { get; set;}
+        public DbSet<Nominas> Nomina { get; set;}
+        public DbSet<User> Users { get; set;}
+    }
+}
--- Models/prueba.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GestionRecursosHumanos.Models
-{
-    public class prueba
-    {
-        public int prueba { get; set; }
-        public string nombre { get; set; }
-    }
-}
