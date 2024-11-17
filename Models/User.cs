e63b7465bc2b5d7dd126707de759d54e895ab4df
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-16 22:13:17 -0500

    prueba felipeconfiguración modelos y carpeta data

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionRecursosHumanos.Models
+{
+    public class User
+    {
+        public int Id { get; set; } // será la llave primaria
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+        [ValidateNever]
+        public string NameUser { get; set; }
+    }
+}
