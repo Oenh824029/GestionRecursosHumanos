a92b85cc9ba9c5a2d4bbfda5d2d39ccf08ccee6c
LuisMorales-23 <dev01.luis.morales@gmail.com>
2024-11-22 13:00:15 -0500

    Funcion crear departameto

---
+++ Pages/Departamento/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionRecursosHumanos.Pages.Departamento
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
