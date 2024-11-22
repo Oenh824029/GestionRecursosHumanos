05542155e1135874c92b3e5d7060233aa04659eb
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-21 22:00:21 -0500

    index de beneficios

---
+++ Pages/Beneficio/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using GestionRecursosHumanos.Data;
+using GestionRecursosHumanos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionRecursosHumanos.Pages.Beneficio
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GestionRecursosContext _context;
+
+        public IndexModel(GestionRecursosContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Beneficios> Beneficio { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Beneficio != null)
+            {
+                Beneficio = await _context.Beneficio.ToListAsync();
+            }
+        }
+
+    }
+}
+
